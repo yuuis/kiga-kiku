a50f1518d81515daf8a5f86df64c86281db0b1d5
Yu Ishiguro <yuuis.egr@gmail.com>
2019-04-27 13:36:24 +0900

    add entities

--- lib/kiku/entities/shop.rb
+++ lib/kiku/entities/shop.rb
@@ -1,1 +1,9 @@
+class Shop < Hanami::Entity
+  attributes do
+    attribute :id, Types::Int
+    attribute :name, Types::String
+    attribute :address, Types::String
+    attribute :genre_code, Types::Int
+  end
+end
 
--- lib/kiku/entities/shop_genre.rb
+++ lib/kiku/entities/shop_genre.rb
@@ -1,1 +1,7 @@
+class ShopGenre < Hanami::Entity
+  attributes do
+    attribute :code, Types::Int
+    attribute :name, Types::String
+  end
+end
 
