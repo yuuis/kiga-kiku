143d05a0f49946c4e03a314b39acabece1587955
Yu Ishiguro <yuuis.egr@gmail.com>
2019-05-01 17:25:53 +0900

    add repositories

--- lib/kiku/interactors/recommend_shop.rb
+++ lib/kiku/interactors/recommend_shop.rb
@@ -1,1 +1,23 @@
+# frozen_string_literal: true
 
+require 'hanami/interactor'
+
+class AddBook
+  include Hanami::Interactor
+
+  expose :shops
+
+  def initialize
+    # set up the object
+  end
+
+  def call(params)
+
+  end
+
+  private
+
+  def recommend(day_of_the_week, user = nil, lat = nil, lng = nil)
+
+  end
+end
--- lib/kiku/repositories/recommended_shop_repository.rb
+++ lib/kiku/repositories/recommended_shop_repository.rb
@@ -1,1 +1,4 @@
+# frozen_string_literal: true
 
+class RecommendedShopRepository < Hanami::Repository
+end
