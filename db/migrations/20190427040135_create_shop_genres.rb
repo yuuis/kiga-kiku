a1a2d2b7285252cfd431ab82d9ec971c58cc48ec
Yu Ishiguro <yuuis.egr@gmail.com>
2019-05-03 20:13:15 +0900

    fix primary key of tables

--- db/migrations/20190427040135_create_shop_genres.rb
+++ db/migrations/20190427040135_create_shop_genres.rb
@@ -1,8 +1,7 @@
 Hanami::Model.migration do
   change do
     create_table :shop_genres do
-      primary_key :code
-
+      column :code, String, null: false, primary_key: true
       column :name, String, null: false
 
       column :created_at, DateTime, null: false
