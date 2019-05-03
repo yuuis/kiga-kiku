b12b444b3942615c193a03bfa0bca445d0849515
Yu Ishiguro <yuuis.egr@gmail.com>
2019-05-03 20:14:27 +0900

    add seeds of genres and small_areas

--- db/seeds/shop_genre.rb
+++ db/seeds/shop_genre.rb
@@ -1,1 +1,20 @@
+require_relative '../../config/boot'
+require 'net/http'
+require 'json'
+require 'uri'
 
+uri = URI.parse(ENV['HOTPEPPER_HOST'])
+http = Net::HTTP.new(uri.host, uri.port)
+
+params = {
+  key: ENV['HOTPEPPER_API_KEY'],
+  format: 'json'
+}
+
+response = http.get(uri.path + '/genre/v1?' + URI.encode_www_form(params))
+
+puts "cant get genres. received: #{response.code}" if response.code != '200'
+
+JSON.parse(response.body)['results']['genre'].each do |genre|
+  ShopGenreRepository.new.create(code: genre['code'], name: genre['name'])
+end
