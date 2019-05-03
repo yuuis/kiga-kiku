9b99f298a372ec913209ba3f51d037d5355a7c21
Yu Ishiguro <yuuis.egr@gmail.com>
2019-05-03 23:52:44 +0900

    add api controller and view

--- apps/api/controllers/shops/recommend.rb
+++ apps/api/controllers/shops/recommend.rb
@@ -1,1 +1,28 @@
+# frozen_string_literal: true
 
+module Api::Controllers::Shops
+  class Recommend
+    include Api::Action
+    accept :json
+
+    expose :shops
+    before :configure_response
+
+    params do
+      required(:user_id).filled
+      required(:words).filled
+    end
+
+    def call(params)
+      halt 400 unless params.valid?
+
+      @shops = RecommendShop.new.call(params.get(:user_id), params.get(:words).split(','))
+    end
+
+    private
+
+    def configure_response
+      self.status = 200
+    end
+  end
+end
