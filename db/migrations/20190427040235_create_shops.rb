432a5210c6a63f69ef5d0f954adae8e01b0f6ef9
Yu Ishiguro <yuuis.egr@gmail.com>
2019-05-10 01:57:53 +0900

    add migrations

--- db/migrations/20190427040235_create_shops.rb
+++ db/migrations/20190427040235_create_shops.rb
@@ -1,13 +1,47 @@
 Hanami::Model.migration do
   change do
     create_table :shops do
-      primary_key :id
+      column :id, String, null: false, primary_key: true
 
       foreign_key :genre_code, :shop_genres, on_delete: :cascade, null: false, key: :code, type: String
+      foreign_key :sub_genre_code, :shop_genres, on_delete: :cascade, null: false, key: :code, type: String
+      foreign_key :large_area_code, :large_areas, on_delete: :cascade, null: false, key: :code, type: String
       foreign_key :small_area_code, :small_areas, on_delete: :cascade, null: false, key: :code, type: String
+      foreign_key :budget_code, :budgets, ondelete: :cascade, null: false, key: :code, type: String
+      foreign_key :service_area_code, :service_areas, ondelete: :cascade, null: false, key: :code, type: String
 
-      column :code, String, null: false
+      column :mobile_access, String, null: false
+      column :address, String, null: false
+      column :lng, String, null: false
+      column :lat, String, null: false
+      column :cource, String, null: false
+      column :show, String, null: false
+      column :non_smoking, String, null: false
+      column :horigotatsu, String, null: false
       column :name, String, null: false
+      column :open, String, null: false
+      column :card, String, null: false
+      column :tatami, String, null: false
+      column :charter, String, null: false
+      column :wifi, String, null: false
+      column :shop_detail_memo, String, null: false
+      column :band, String, null: false
+      column :karaoke, String, null: false
+      column :midnight, String, null: false
+      column :urls, String, null: false
+      column :english, String, null: false
+      column :lunch, String, null: false
+      column :close, String, null: false
+      column :budget_memo, String, null: false
+      column :tv, String, null: false
+      column :private_room, String, null: false
+      column :barrier_free, String, null: false
+      column :child, String, null: false
+      column :capacity, String, null: false
+      column :pet, String, null: false
+      column :free_food, String, null: false
+      column :free_drink, String, null: false
+      column :station_name, String, null: false
 
       column :created_at, DateTime, null: false
       column :updated_at, DateTime, null: false
--- db/migrations/20190427040752_create_recommended_shops.rb
+++ db/migrations/20190427040752_create_recommended_shops.rb
@@ -2,7 +2,7 @@ Hanami::Model.migration do
   change do
     create_table :recommended_shops do
       primary_key :id
-      foreign_key :shop_id, :shops, on_delete: :cascade, null: false
+      foreign_key :shop_id, :shops, on_delete: :cascade, null: false, type: String
       foreign_key :recommend_log_id, :recommend_logs, on_delete: :cascade, null: false
 
       column :feed_back, Integer
