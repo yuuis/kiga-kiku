8d63447afff1c5945a2c59b562ef6f8eecaec556
Yu Ishiguro <yuuis.egr@gmail.com>
2019-05-17 18:08:50 +0900

    add repository for user_went_shops

--- lib/kiku/repositories/user_went_shop_repository.rb
+++ lib/kiku/repositories/user_went_shop_repository.rb
@@ -1,1 +1,15 @@
+# frozen_string_literal: true
 
+class UserWentShopRepository < Hanami::Repository
+  associations do
+    belongs_to :user
+    belongs_to :shop
+  end
+
+  def recent(user, limit: 3)
+    user_went_shops
+      .where(user_id: user.id)
+      .order(:created_at)
+      .limit(limit)
+  end
+end
