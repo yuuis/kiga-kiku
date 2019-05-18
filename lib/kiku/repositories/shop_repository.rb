86629d47d052176c3265c7a9241d62393177181c
Yu Ishiguro <yuuis.egr@gmail.com>
2019-05-18 11:55:20 +0900

    add association to budget

--- lib/kiku/repositories/shop_repository.rb
+++ lib/kiku/repositories/shop_repository.rb
@@ -8,11 +8,6 @@ class ShopRepository < Hanami::Repository
     # belongs_to :small_area
     # belongs_to :large_area
     # belongs_to :service_area
-    # belongs_to :budget
-  end
-
-  def user_recent(user)
-    # userが行った一番最近のお店を返す。
-    # 行ったをどうやって判断する？
+    belongs_to :budget
   end
 end
