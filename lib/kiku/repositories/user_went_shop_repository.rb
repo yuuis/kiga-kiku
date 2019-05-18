e131708d822dd05825326d4f58519f6e57452ae3
Yu Ishiguro <yuuis.egr@gmail.com>
2019-05-18 13:27:44 +0900

    add interactor

--- lib/kiku/repositories/user_went_shop_repository.rb
+++ lib/kiku/repositories/user_went_shop_repository.rb
@@ -8,8 +8,10 @@ class UserWentShopRepository < Hanami::Repository
 
   def recent(user, limit: 3)
     user_went_shops
+      .select(:shop_id)
       .where(user_id: user.id)
       .order(:created_at)
       .limit(limit)
+      .to_a
   end
 end
