aba35a2144d7761db5ec58cdb23c89ea47ed2ea8
Yu Ishiguro <yuuis.egr@gmail.com>
2019-05-18 16:22:56 +0900

    add routes

--- apps/api/config/routes.rb
+++ apps/api/config/routes.rb
@@ -2,3 +2,5 @@
 
 get '/shops', to: 'shops#recommend'
 get '/went_shops/', to: 'shops#user_went'
+get '/guess_like/', to: 'conditions#guess_like'
+get '/guess_dislike/', to: 'conditions#guess_dislike'
