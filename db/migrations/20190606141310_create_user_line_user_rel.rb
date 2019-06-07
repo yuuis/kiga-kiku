3062469976acfe745547068cce2b86943133952e
Yu Ishiguro <yuuis.egr@gmail.com>
2019-06-07 13:21:48 +0900

    add line_usr_rel table

---
+++ db/migrations/20190606141310_create_user_line_user_rel.rb
@@ -0,0 +1,13 @@
+Hanami::Model.migration do
+  change do
+    create_table :user_line_account_rel do
+      primary_key :id
+      foreign_key :user_id, :users, on_delete: :cascade, null: false
+
+      column :line_user_id, String, null: false
+
+      column :created_at, DateTime, null: false
+      column :updated_at, DateTime, null: false
+    end
+  end
+end
