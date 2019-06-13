224bfff0f755322cce035fdb2293ae8e2be88978
Yu Ishiguro <yuuis.egr@gmail.com>
2019-06-13 14:30:03 +0900

    add method for find_by_user_id

--- lib/kiku/repositories/recommend_transaction_repository.rb
+++ lib/kiku/repositories/recommend_transaction_repository.rb
@@ -5,4 +5,12 @@ class RecommendTransactionRepository < Hanami::Repository
     belongs_to :user
     has_many :recommend_conversations
   end
+
+  def find_by_user_id(user_id)
+    transaction_expire_time = 15
+    recommend_transaction = recommend_transactions.where(user_id: user_id).last
+    return nil if (Time.now - recommend_transaction) / 60 >  transaction_expire_time
+
+    recommend_transaction
+  end
 end
