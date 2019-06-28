48a5642d1b7da2a6b9f19f24d81b0106e685086c
Yu Ishiguro <yuuis.egr@gmail.com>
2019-06-28 15:10:37 +0900

    add fint_by_line_user_id

--- lib/kiku/repositories/user_line_user_rel_repository.rb
+++ lib/kiku/repositories/user_line_user_rel_repository.rb
@@ -4,4 +4,8 @@ class UserLineUserRelRepository < Hanami::Repository
   associations do
     belongs_to :user
   end
+
+  def find_by_line_user_id (line_user_id)
+    user_line_user_rel.where(line_user_id: line_user_id).map_to(UserLineUserRel).to_a.first
+  end
 end
