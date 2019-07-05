548872f2a17535c7658d4d5c9c0d8fc75b8aa25a
sho <sskmy1024.y@gmail.com>
2019-07-05 12:53:53 +0900

    some fix

--- apps/line_bot/controllers/callback/create_reply_message.rb
+++ apps/line_bot/controllers/callback/create_reply_message.rb
@@ -60,9 +60,9 @@ class CreateReplyMessage < LineManager
     transaction = recommend_transaction_repository.create(user_id: user_id) if transaction.nil?
 
     if watson_entities.include?('精度向上キーワード')
-      words = ['ラーメン'] # TODO:前回のワードを取得
+      words = ['ラーメン'] # TODO: 前回のワードを取得
 
-      user_request = 'もっと安い' # TODO:ユーザが送ってきた要望「もっと**」
+      user_request = 'もっと安い' # TODO: ユーザが送ってきた要望「もっと**」
       past_conditions = ConditionRepository.new.condition_checks(user_request, JSON.parse(conversation.conditions, symbolize_names: true))
     elsif watson_entities.include?('メニュー')
       words = get_origin_entities(@user_message, @watson_result, 'メニュー') # watsonのメニューに引っかかった
@@ -77,11 +77,11 @@ class CreateReplyMessage < LineManager
       longitude = location.longitude
     end
 
-    recommend = RecommendShop.new.call(self.user_id, words, latitude, longitude, past_conditions)
+    recommend = RecommendShop.new.call(user_id, words, latitude, longitude, past_conditions)
     shops = recommend.recommend_result[:shops]
     conditions = recommend.recommend_result[:conditions]
 
-    return cannot_found_recommend_shop if shops.length < 1
+    return cannot_found_recommend_shop if shops.length.empty?
 
     recommend_conversation_repository.create(recommend_transaction_id: transaction[:id], conditions: conditions.to_json, user_word: @user_message, bot_word: reply_message_text)
     @reply_message << render_shops_template(shops).merge(get_more_condition)
@@ -218,6 +218,4 @@ class CreateReplyMessage < LineManager
       }
     ]
   end
-
-
 end
