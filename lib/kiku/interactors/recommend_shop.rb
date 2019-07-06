6403f5c7d28bd134534929e048e45696dbd6a41e
sho <sskmy1024.y@gmail.com>
2019-07-06 20:02:34 +0900

    add timely recommend keywords

--- lib/kiku/interactors/recommend_shop.rb
+++ lib/kiku/interactors/recommend_shop.rb
@@ -11,8 +11,9 @@ class RecommendShop
   # TODO: とりあえず緯度/経度は八王子にする
   def call(user_id, words = [], latitude = '35.65562', longitude = '139.3366642', past_conditions = nil)
     user = UserRepository.new.find(user_id)
-
     return nil if user.nil?
+
+    words = get_timely_keywords unless words.empty? && past_conditions.nil?
 
     @recommend_result = recommend(user, words, latitude, longitude, past_conditions)
   end
@@ -80,6 +81,20 @@ class RecommendShop
     end
 
     conditions
+  end
+
+  # 時間帯によってキーワードを返す
+  # TODO: 履歴やユーザの好みによってキーワードを最適化
+  def get_timely_keywords
+    words = []
+    case Time.now.hour
+    when 22..24, 0..3 then words << ["お酒", "居酒屋"].sample
+    when 4..10 then words << ["モーニング", "コーヒー", "ブレックファースト", "朝食", "朝ごはん"].sample
+    when 11..13 then words << ["ラーメン", "ランチ", "カレー", "パスタ", "お昼ご飯"].sample
+    when 14..17 then words << ["ケーキ", "お菓子", "おやつ", "和菓子", "紅茶", "喫茶店"].sample
+    when 18..21 then words << ["ディナー", "夜ご飯", "夕食", "レストラン"].sample
+    else words << ["焼肉"].sample
+    end
   end
 
   def get_shops(conditions)
