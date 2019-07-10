acfbc4e911e20e3ae613ed1e624f48317d3bd3d9
sho <sskmy1024.y@gmail.com>
2019-07-10 15:37:59 +0900

    support line client simulator

--- apps/line_bot/controllers/callback/line_manager.rb
+++ apps/line_bot/controllers/callback/line_manager.rb
@@ -8,13 +8,14 @@ class LineManager
     @client ||= Line::Bot::Client.new do |config|
       config.channel_secret = ENV['LINE_CHANNEL_SECRET']
       config.channel_token = ENV['LINE_CHANNEL_TOKEN']
+      config.endpoint = 'http://host.docker.internal:8080' if check_dummy_token(@request)
     end
   end
 
   def initialize(body)
     @request = body
     @events = client.parse_events_from(body)
-
+    
     @line_id = @events.first['source']['userId']
     @profile = JSON.parse(client.get_profile(@line_id).read_body)
     @display_name = @profile['displayName']
@@ -36,5 +37,10 @@ class LineManager
   def user_register
     user = UserRepository.new.create(name: @display_name)
     UserLineUserRelRepository.new.create(user_id: user.id, line_user_id: @line_id)
+  end
+
+  private
+  def check_dummy_token(body)
+    JSON.parse(body)['events'].first['replyToken'] === "dummyToken"
   end
 end
