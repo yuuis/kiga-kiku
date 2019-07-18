02880aff67c65e0b09b543af8afcffcf93d0a093
Yu Ishiguro <yuuis.egr@gmail.com>
2019-07-18 17:44:25 +0900

    add methods

--- db/migrations/20190427040752_create_recommended_shops.rb
+++ db/migrations/20190427040752_create_recommended_shops.rb
@@ -2,9 +2,9 @@ Hanami::Model.migration do
   change do
     create_table :recommended_shops do
       primary_key :id
-      foreign_key :shop_id, :shops, on_delete: :cascade, null: false, type: String
       foreign_key :recommend_conversation_id, :recommend_conversations, on_delete: :cascade, null: false
 
+      column :shop_id, String, null: false
       column :created_at, DateTime, null: false
       column :updated_at, DateTime, null: false
     end
