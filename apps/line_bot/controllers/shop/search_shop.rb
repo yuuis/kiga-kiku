aa8798d9466e253cdb66a3627db75191e69ba663
sho <sskmy1024.y@gmail.com>
2019-12-11 10:59:29 +0900

    fix search shop no iroiro

--- apps/line_bot/controllers/shop/search_shop.rb
+++
@@ -1,8 +0,0 @@
-class SearchShop
-  def last_user_went_shop(user)
-    return nil if user.nil?
-
-    user_whent_shop = UserWentShopRepository.new.recent(user, limit: 1)
-    ShopRepository.new.find(user_whent_shop.first.shop_id) unless user_whent_shop.blank?
-  end
-end
